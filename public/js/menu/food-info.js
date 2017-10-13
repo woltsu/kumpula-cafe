df64c6834f5b17bc05845c63778a69f872b859d0
olli <oltsuwh@gmail.com>
2017-10-13 12:19:16 +0300

    trying to fix the expanded height

--- public/js/menu/food-info.js
+++ public/js/menu/food-info.js
@@ -16,9 +16,6 @@ class Info extends React.Component {
         var divHeight = document.getElementById(this.props.index).clientHeight;
         var nameHeight = document.getElementById(nameId).clientHeight;
         var expandedHeight = document.getElementById(infoId).clientHeight;
-        if (expandedHeight != 0) {
-            expandedHeight = divHeight + nameHeight;
-        }
         this.setState({
             height: 0,
             expandedHeight: expandedHeight
